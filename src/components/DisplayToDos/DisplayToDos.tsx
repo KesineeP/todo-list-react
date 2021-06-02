424a38a9379a02a6e3a027ba739210cc3d860956
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-06-02 12:52:42 -0400

    Added Typescript to DisplayToDos Components

---
+++ src/components/DisplayToDos/DisplayToDos.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, SetStateAction } from "react";
+import Todo from "./Todo";
+import CSS from "csstype";
+
+interface Props {
+  database: TodoType[];
+  setDatabase: Dispatch<SetStateAction<TodoType[]>>;
+  todo: any;
+}
+type TodoType = {
+  id: string;
+  data: string;
+  isChecked: boolean;
+};
+const DisplayToDos: React.FC<Props> = ({ database, setDatabase }) => {
+  return (
+    <div style={styles.container}>
+      {database.map((item, index) => (
+        <Todo
+          todo={item}
+          key={index}
+          database={database}
+          setDatabase={setDatabase}
+        />
+      ))}
+    </div>
+  );
+};
+
+type Style = {
+  container: CSS.Properties;
+};
+const styles: Style = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+  },
+};
+export default DisplayToDos;
